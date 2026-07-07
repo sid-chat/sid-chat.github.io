5e9e9eff60a6f65458c89acaeade0b0d4416c706
sid-chat <sidchat97@gmail.com>
2026-07-08 00:07:45 +0200

    Update cv-data.js

--- cv-data.js
+++ cv-data.js
@@ -9,7 +9,7 @@ window.CV_DATA = {
   shortSummary:
     "Complex Adaptive Systems,\nAgent-based Models,\nCollective & Diverse Intelligence, \nJAX",
   about:
-    "Namaste and welcome to my website. \n\nProfessionally: I am a mechanical and control engineer who began doing a PhD in the field of AI and is now concluding it in the field of complex systems. \n\n Not Professionally: Robot-maker, Guitar-player, Blog-writer, Hike-master, Salsa-dancer, Slow-swimmer",
+    "Namaste and welcome to my website. \n\nProfessionally: I am a mechanical and control engineer who began doing a PhD in the field of AI and is now concluding it in the field of complex systems. \n\n Not Professionally: Robot-maker, Guitar-player, Blog-writer, Hike-master, Salsa-dancer, Slow-swimmer, Animal-lover",
 
   links: [
     { label: "Email", url: "mailto:siddharth.chaturvedi@donders.ru.nl" },
