76ca28a59ed4694a1a66a5eacb27ae351fa96a6c
sid-chat <sidchat97@gmail.com>
2026-07-07 22:56:50 +0200

    Update cv-data.js

--- cv-data.js
+++ cv-data.js
@@ -4,7 +4,7 @@
 window.CV_DATA = {
   name: "Siddharth Chaturvedi",
   initials: "SC",
- role: "PhD candidate,\nArtificial Cognitive Systems lab",
+  role: "PhD candidate,\nArtificial Cognitive Systems lab",
   location: "Radboud University",
   shortSummary:
     "I build computational models and software to study complex adaptive systems, focused on collective and diverse intelligence.",
